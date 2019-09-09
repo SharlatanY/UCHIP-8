b9d99d9d29cd006fa30a053ddaf158ce0742e248
Sharlatan <12735825+SharlatanY@users.noreply.github.com>
2019-09-09 17:31:28 +0200

    Project setup, adding texture drawing to screen
    Set up project, added texture drawing directly to screen

---
+++ UCHIP-8/Assets/Scripts/ScreenRenderer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+
+namespace Chip8
+{
+  public class ScreenRenderer : MonoBehaviour
+  {
+    public Texture ScreenTexture;
+    public float x, y, width, height;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+
+
+
+    private void OnPostRender()
+    {
+      GL.PushMatrix();
+      GL.LoadPixelMatrix(0, Screen.width, Screen.height, 0);
+
+      Graphics.DrawTexture(
+        new Rect(x, y, Screen.width, Screen.height), //todo replace with correct coordinates depending on aspect ration, resolution etc
+        ScreenTexture);
+      GL.PopMatrix();
+    }
+
+    private bool ResolutionChanged()
+    {
+      throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Recalculates the coordinates used to correctly draw the texture
+    /// </summary>
+    private void RecalculateTextureDrawCoordinates()
+    {
+      //todo find nearest multiple of 64x32 for screen resolution and calculate exact values for drawing screen (texture)
+      throw new NotImplementedException();
+    }
+  }
+}
