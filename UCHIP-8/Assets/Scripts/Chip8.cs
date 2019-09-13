4cf8d351b44aa6c25d16b633fb52d6cd79ce8033
Sharlatan <12735825+SharlatanY@users.noreply.github.com>
2019-09-13 12:58:24 +0200

    Writing font to RAM

--- UCHIP-8/Assets/Scripts/Chip8.cs
+++ UCHIP-8/Assets/Scripts/Chip8.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using UnityEngine;
 
 namespace Chip8
@@ -13,8 +14,47 @@ namespace Chip8
 
     //RAM
     private static readonly byte[] RAM = new byte[4096]; //0x0 to 0xFFF
+    private const ushort FontStartingAddress = 0x0;
 
     //Program counter
-    private static ushort PC; //stores current execution address
+    private static ushort pcStartingAddress = 0x200;
+    private static ushort PC = pcStartingAddress; //stores current execution address
+
+    //Misc
+    private static float _clock = 500;
+
+    private void Start()
+    {
+      InitializeRAM();
+    }
+
+    private void Reset()
+    {
+      InitializeRAM();
+      PC = pcStartingAddress;
+
+      //todo add other stuff to be reset as development progresses
+    }
+
+    private void InitializeRAM()
+    {
+      for (var i = 0; i < RAM.Length; i++)
+      {
+        RAM[i] = 0x0;
+      }
+
+      WriteFontIntoRAM(FontStartingAddress);
+    }
+
+    /// <summary>
+    /// Writes the font sprites into the RAM
+    /// </summary>
+    /// <param name="startingAddress">Address at which first byte will be written</param>
+    private void WriteFontIntoRAM(ushort startingAddress)
+    {
+      Array.Copy(Chip8Constants.Font, 0, RAM, (int)startingAddress, Chip8Constants.Font.Length );
+    }
+
+
   }
 }
